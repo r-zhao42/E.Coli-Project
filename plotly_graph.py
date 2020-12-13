dce2ef34c99e05b9cd4a1aebfbf7debeb908a2e2
jojoonthat <jojoonthat@users.noreply.github.com>
2020-12-13 17:16:22 -0500

    ploty added

---
+++ plotly_graph.py
@@ -0,0 +1,50 @@
+import sklearn
+import yourmomssklearn
+import misc1
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+
+df = yourmomssklearn.get_total_data(2010, 2030)
+df2 = pd.read_excel('totals.xlsx')
+name = 'UK E.coli vs. Temperature'
+
+
+
+
+fig = go.Figure()
+
+# Add traces
+fig.add_trace(go.Scatter(x=df['years'], y=df['ecoli'],
+                         name="Past E.coli data",
+                         mode='lines + markers',
+                         marker=dict(
+                             size=10,
+                             color=df['ecoli'],
+                             showscale=True),
+                         ))
+
+
+
+fig.add_trace(go.Scatter(x=df2['x'], y=df2['y'],
+                         name='Projected E.coli data',
+                         mode='lines + markers',
+                         marker=dict(
+                             size=10,
+                             color=df2['y'],  # set color equal to a variable
+                             showscale=True)))
+
+fig.update_layout(title={
+    'text': name,
+    'y': 0.9,
+    'x': 0.5,
+    'xanchor': 'center',
+    'yanchor': 'top'},
+    font=dict(family="Helvetica", size=18),
+    barmode='stack',
+    legend_orientation="h",
+
+)
+
+
+fig.show()
---
+++ plotly_map.py
@@ -0,0 +1,97 @@
+import plotly.graph_objects as go
+import pandas as pd
+
+mapbox_access_token = 'pk.eyJ1Ijoiam9qb29udGhhdCIsImEiOiJja2lta3UzbnowYWRtMzVud3NrNjI3N2JjIn0.kYIFPU3HJbjDsNYyQFaGdA'
+df = pd.read_csv(
+    'jo.csv')
+site_lat = df.lat
+site_lon = df.lon
+locations_name = df.text
+
+fig = go.Figure()
+
+fig.add_trace(go.Scattermapbox(
+    lat=site_lat,
+    lon=site_lon,
+    name='Weather station',
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=20,
+        color='rgb(255, 218, 200)',  # percentage
+        opacity=0.75
+    ),
+    text=locations_name,
+    hoverinfo='text',
+    hoverlabel=dict(
+        bgcolor='rgb(255, 217, 255)',
+        font_size=40,
+        font_family="Helvetica"
+    )
+))
+
+fig.add_trace(go.Scattermapbox(
+    lat=site_lat,
+    lon=site_lon,
+    name='',
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=14,
+        color='rgb(211, 237, 255)', #percentage
+        opacity=0.75
+    ),
+    text=locations_name,
+    hoverinfo='text',
+    hoverlabel=dict(
+        bgcolor='rgb(182, 252, 213)',
+        font_size=40,
+        font_family="Helvetica"
+)
+))
+
+fig.add_trace(go.Scattermapbox(
+    lat=site_lat,
+    lon=site_lon,
+    name='',
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=7,
+        color='rgb(182, 252, 213)',  # percentage
+        opacity=0.8
+    ),
+    text=locations_name,
+    hoverinfo='text',
+    hoverlabel=dict(
+        bgcolor='rgb(182, 252, 213)',
+        font_size=14,
+        font_family="Helvetica"
+    )
+))
+
+
+fig.update_layout(
+    title={
+        'text': 'Number of E.coli Cases in all Weather Stations in the UK',
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+              autosize=True,
+    hovermode='closest',
+    font=dict(family="Helvetica", size=18),
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=dict(
+            lat=52,
+            lon=0.12
+        ),
+        pitch=10,
+        zoom=3,
+        style='dark'
+    )
+)
+
+
+fig.update_traces(showlegend=True, selector=dict(type='scattermapbox'))
+
+fig.show()
