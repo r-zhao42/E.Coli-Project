ab1e5695156f1789442a21133db1fbf9f5a4bea4
jojoonthat <jojoonthat@users.noreply.github.com>
2020-12-13 22:04:40 -0500

    plotly map donzo

--- plotly_map.py
+++ plotly_map.py
@@ -1,27 +1,34 @@
+"""
 import plotly.graph_objects as go
 import pandas as pd
+import yourmomssklearn
+
 
 mapbox_access_token = 'pk.eyJ1Ijoiam9qb29udGhhdCIsImEiOiJja2lta3UzbnowYWRtMzVud3NrNjI3N2JjIn0.kYIFPU3HJbjDsNYyQFaGdA'
 df = pd.read_csv(
     'jo.csv')
-site_lat = df.lat
-site_lon = df.lon
-locations_name = df.text
+site_lat = tuple(df.lat.tolist())
+site_lon = tuple(df.lon.tolist())
+locations_name = tuple(df.text.tolist())
+total = yourmomssklearn.get_percentage_increase_all_stations(2020, 2100)
+print(type(total['waddington']))
+print(type(site_lat), type(site_lon), type(locations_name))
+
 
 fig = go.Figure()
 
 fig.add_trace(go.Scattermapbox(
     lat=site_lat,
     lon=site_lon,
-    name='Weather station',
+    name='cardiff',
     mode='markers',
     marker=go.scattermapbox.Marker(
         size=20,
         color='rgb(255, 218, 200)',  # percentage
         opacity=0.75
     ),
-    text=locations_name,
-    hoverinfo='text',
+    text=total['cardiff'],
+    hoverinfo='name+text',
     hoverlabel=dict(
         bgcolor='rgb(255, 217, 255)',
         font_size=40,
@@ -32,15 +39,15 @@ fig.add_trace(go.Scattermapbox(
 fig.add_trace(go.Scattermapbox(
     lat=site_lat,
     lon=site_lon,
-    name='',
+    name='cardiff',
     mode='markers',
     marker=go.scattermapbox.Marker(
         size=14,
         color='rgb(211, 237, 255)', #percentage
         opacity=0.75
     ),
-    text=locations_name,
-    hoverinfo='text',
+    text=total['cardiff'],
+    hoverinfo='name+text',
     hoverlabel=dict(
         bgcolor='rgb(182, 252, 213)',
         font_size=40,
@@ -51,15 +58,15 @@ fig.add_trace(go.Scattermapbox(
 fig.add_trace(go.Scattermapbox(
     lat=site_lat,
     lon=site_lon,
-    name='',
+    name='cardiff',
     mode='markers',
     marker=go.scattermapbox.Marker(
         size=7,
         color='rgb(182, 252, 213)',  # percentage
         opacity=0.8
     ),
-    text=locations_name,
-    hoverinfo='text',
+    text=total['cardiff'],
+    hoverinfo='name+text',
     hoverlabel=dict(
         bgcolor='rgb(182, 252, 213)',
         font_size=14,
@@ -91,6 +98,117 @@ fig.update_layout(
     )
 )
 
+
+fig.update_traces(showlegend=True, selector=dict(type='scattermapbox'))
+
+fig.show()
+"""
+
+import plotly.graph_objects as go
+import pandas as pd
+import yourmomssklearn
+
+start = 2020
+end = 2100
+
+total = yourmomssklearn.get_percentage_increase_all_stations(start, end)
+
+mapbox_access_token = 'pk.eyJ1Ijoiam9qb29udGhhdCIsImEiOiJja2lta3UzbnowYWRtMzVud3NrNjI3N2JjIn0.kYIFPU3HJbjDsNYyQFaGdA'
+df = pd.read_csv(
+    'jo.csv')
+site_lat = df.lat
+site_lon = df.lon
+locations_name = df.text.tolist()
+
+for i in range(len(locations_name)):
+    location = locations_name[i]
+    locations_name[i] = "{}: Projected to have {:.4f}% of increase from {} to {}".format(location.title(), total[location], start, end)
+# print(locations_name)
+# print(type(locations_name))
+# print(total)
+# print(type(total))
+
+fig = go.Figure()
+
+fig.add_trace(go.Scattermapbox(
+    lat=site_lat,
+    lon=site_lon,
+    name='Weather station',
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=20,
+        color='rgb(255, 218, 200)',  # percentage
+        opacity=0.75
+    ),
+    text=locations_name,
+    hoverinfo='text',
+    hoverlabel=dict(
+        bgcolor='rgb(255, 217, 255)',
+        font_size=40,
+        font_family="Helvetica"
+    )
+))
+
+fig.add_trace(go.Scattermapbox(
+    lat=site_lat,
+    lon=site_lon,
+    name='',
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=14,
+        color='rgb(211, 237, 255)',  # percentage
+        opacity=0.75
+    ),
+    text=locations_name,
+    hoverinfo='text',
+    hoverlabel=dict(
+        bgcolor='rgb(182, 252, 213)',
+        font_size=40,
+        font_family="Helvetica"
+    )
+))
+
+fig.add_trace(go.Scattermapbox(
+    lat=site_lat,
+    lon=site_lon,
+    name='',
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=7,
+        color='rgb(182, 252, 213)',  # percentage
+        opacity=0.8
+    ),
+    text=locations_name,
+    hoverinfo='text',
+    hoverlabel=dict(
+        bgcolor='rgb(182, 252, 213)',
+        font_size=14,
+        font_family="Helvetica"
+    )
+))
+
+fig.update_layout(
+    title={
+        'text': 'Number of E.coli Cases in all Weather Stations in the UK',
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+    autosize=True,
+    hovermode='closest',
+    font=dict(family="Helvetica", size=18),
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=dict(
+            lat=52,
+            lon=0.12
+        ),
+        pitch=10,
+        zoom=3,
+        style='dark'
+    )
+)
 
 fig.update_traces(showlegend=True, selector=dict(type='scattermapbox'))
 
