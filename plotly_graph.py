5d30300774f87fc2ce991dcc0f30795ff58e4634
Cliff Zhang <cliffzhang08@gmail.com>
2020-12-14 00:31:12 -0500

    moms

--- plotly_graph.py
+++ plotly_graph.py
@@ -6,38 +6,43 @@ import yourmomssklearn
 import plotly.graph_objects as go
 import pandas as pd
 
-df = yourmomssklearn.get_total_data(2010, 2030)
-df2 = pd.read_excel('totals.xlsx')
-NAME_ = 'UK E.coli Projection Until 2100'
+def plot_your_mom() -> None:
+    """
+    It plots two things:
+        - projected weight of your mother over time
+        - her weight history
+    """
+    df = yourmomssklearn.get_total_data(2010, 2030)
+    df2 = pd.read_excel('totals.xlsx')
+    NAME_ = 'UK E.coli Projection Until 2100'
 
-df['years'] = df['years'].map("{}-01-01".format)
+    df['years'] = df['years'].map("{}-01-01".format)
 
-fig = go.Figure()
+    fig = go.Figure()
 
-# Add traces
-fig.add_trace(go.Scatter(x=df['years'], y=df['ecoli'],
-                         name="Projected E.coli data",
-                         mode='lines + markers',
-                         marker=dict(
-                             size=10,
-                             color=df['ecoli'],
-                             showscale=True),
-                         ))
-
-
-fig.add_trace(go.Scatter(x=df2['x'], y=df2['y'],
-                         name='Past E.coli data',
-                         mode='lines + markers'))
-
-
-fig.update_layout(title={'text': NAME_,
-                         'y': 0.9,
-                         'x': 0.5,
-                         'xanchor': 'center',
-                         'yanchor': 'top'},
-                  font=dict(family="Helvetica", size=18),
-                  barmode='stack',
-                  legend_orientation="h")
+    # Add traces
+    fig.add_trace(go.Scatter(x=df['years'], y=df['ecoli'],
+                            name="Projected E.coli data",
+                            mode='lines + markers',
+                            marker=dict(
+                                size=10,
+                                color=df['ecoli'],
+                                showscale=True),
+                            ))
 
 
-fig.show()
+    fig.add_trace(go.Scatter(x=df2['x'], y=df2['y'],
+                            name='Past E.coli data',
+                            mode='lines + markers'))
+
+
+    fig.update_layout(title={'text': NAME_,
+                            'y': 0.9,
+                            'x': 0.5,
+                            'xanchor': 'center',
+                            'yanchor': 'top'},
+                    font=dict(family="Helvetica", size=18),
+                    barmode='stack',
+                    legend_orientation="h")
+
+    fig.show()
